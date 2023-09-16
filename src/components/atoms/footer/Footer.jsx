638ccb4062ea13ab727518919aba82f789d9106f
helenaponted <helenaponte@hotmail.es>
2023-09-16 15:18:43 +0100

    smaller footer

--- src/components/atoms/footer/Footer.jsx
+++ src/components/atoms/footer/Footer.jsx
@@ -10,11 +10,11 @@ function Footer() {
 
 <footer>
     <div className="bg-blue-300">
-    <div className="max-w-2xl mx-auto text-white py-10">
+    <div className="max-w-2xl mx-auto text-white py-4">
         <div className="text-center">
-            <h3 className="text-3xl mb-3"> Start drinking water today </h3>
+            <h3 className="text-3xl mb-2"> Start drinking water today </h3>
             <p> Stay hydrated. All day, every day. </p>
-            <div className="flex justify-center my-10">
+            <div className="flex justify-center my-4">
                 <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
                     <img src={pinterest} className="w-7 md:w-8"/>
                     <div className="text-left ml-3">
@@ -32,7 +32,7 @@ function Footer() {
             </div>
         </div>
         <div className="mt-10 flex flex-col md:flex-row md:justify-between items-center text-sm text-white">
-            <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy;Aqua Check, 2023. </p>
+            <p className="order-2 md:order-1 mt-2git  md:mt-0"> &copy;Aqua Check, 2023. </p>
             <div className="order-1 md:order-2">
                 <span className="px-2">About us</span>
                 <span className="px-2 border-l">Contact us</span>
