ef822c43db614e8481def5d68978b24c3b0afcd5
helenaponted <helenaponte@hotmail.es>
2023-09-21 19:37:57 +0100

    custom hooks changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css'
 import Navbar from './components/atoms/navbar/Navbar'
 import Homepage from './containers/Homepage'
 import { Routes, Route } from "react-router-dom"
-import useFetch from "./hooks/useFetch"
 import { useEffect, useState } from "react";
 import About from './containers/About'
 import Footer from './components/atoms/footer/Footer'
 import Goal from './containers/Goal'
 import { createContext } from 'react';
+import UseGetRequest from './hooks/UseGetRequest';
 
 export const UserContext = createContext(null);
 
@@ -16,7 +16,7 @@ function App() {
 
   const [user, setUser] = useState(null);
 
-  const { data: userData } = useFetch("http://127.0.0.1:8000/api/users/1");
+  const { data: userData } = UseGetRequest("http://127.0.0.1:8000/api/users/1");
 
   useEffect(() => {
       setUser(userData);
---
+++ src/hooks/UseGetRequest.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const UseGetRequest = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+
+        if(!response.ok) {
+          throw new Error("GET response was not OK");
+        }
+
+        const json = await response.json();
+        setData(json);
+        
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      }
+    }
+
+    fetchData();
+ }, [url]);
+
+  return { data };
+};
+
+export default UseGetRequest;
